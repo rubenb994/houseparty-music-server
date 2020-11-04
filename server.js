b7170e4412e781b1284693a2bcb18130aed2b566
Ruben <r.brugman@student.fontys.nl>
2020-11-04 02:51:07 +0100

    Working exaple of receiving request from client

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });;
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit('test event', 'some test data')
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  socket.on("new-choice", (choice) => {
+    console.log("new choice from", choice);
+  });
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
