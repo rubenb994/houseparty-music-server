022628b2f56bb59a8b88c077ba1c43977ad14ec4
Ruben <r.brugman@student.fontys.nl>
2020-11-05 14:55:25 +0100

    Implemented queuing of votes

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const scopes = [
   "user-read-email",
   "app-remote-control",
   "user-modify-playback-state ",
+  "user-read-playback-state",
 ];
 const state = "some-state-of-my-choice";
 
@@ -36,18 +37,6 @@ function getSpotifyPlaylist() {
   );
 }
 
-function playRandomSong() {
-  spotifyApi.play().then(
-    function () {
-      console.log("Playback started");
-    },
-    function (err) {
-      //if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
-      console.log("Something went wrong!", err);
-    }
-  );
-}
-
 app.get("/login", (req, res) => {
   res.redirect(spotifyApi.createAuthorizeURL(scopes, state, true));
 });
@@ -64,15 +53,14 @@ app.get("/callback", function (req, res) {
       // Set the access token on the API object to use it in later calls
       spotifyApi.setAccessToken(data.body["access_token"]);
       spotifyApi.setRefreshToken(data.body["refresh_token"]);
-
-      playRandomSong();
     },
     function (err) {
-      console.log("Something went wrong!", err);
+      console.log("Something went wrong during login", err);
     }
   );
 });
 
+var firstPersonConnected = false;
 // Websockets stuff below
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
@@ -82,8 +70,77 @@ io.on("connection", (socket) => {
 
     socket.broadcast.emit("new-choice", choice);
   });
+
+  socket.on("setup-vote", () => {
+    setupPlaybackCheck();
+  });
+
+  socket.on("play", () => {
+    spotifyApi.play().then(
+      function () {
+        console.log("Playback started");
+      },
+      function (err) {
+        //if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
+        console.log("Something went wrong!", err);
+      }
+    );
+  });
+
+  socket.on("pause", () => {
+    spotifyApi.pause().then(
+      function () {
+        console.log("Playback paused");
+      },
+      function (err) {
+        //if the user making the request is non-premium, a 403 FORBIDDEN response code will be returned
+        console.log("Something went wrong!", err);
+      }
+    );
+  });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
-});
+var voteBusy = false;
+var voteEnded = false;
+
+function setupPlaybackCheck() {
+  setInterval(() => {
+    spotifyApi.getMyCurrentPlaybackState().then(
+      function (data) {
+        const voteStart = 40000;
+        const voteEnd = 20000;
+        const remainingDuration =
+          data.body.item.duration_ms - data.body.progress_ms;
+
+        if (
+          !voteBusy &&
+          remainingDuration <= voteStart &&
+          remainingDuration > voteEnd
+        ) {
+          voteBusy = true;
+          voteEnded = false;
+          setupVote();
+        }
+
+        if (voteBusy && !voteEnded && remainingDuration <= voteEnd) {
+          voteBusy = false;
+          voteEnded = true;
+          endVote();
+        }
+      },
+      function (err) {
+        console.log("Something went wrong fetching playback info ", err);
+      }
+    );
+  }, 1000);
+}
+
+function setupVote() {
+  console.log("SETUP VOTE");
+}
+
+function endVote() {
+  console.log("END VOTE");
+}
+
+http.listen(3000, () => {});
